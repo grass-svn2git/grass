grass-svn2git/grass

commit 66f086cf62d20eec4a14557951cd709b95fa5630

wxGUI: add barscale and arrow pictures to d.barscale gui (see #2077)
git-svn-id: https://svn.osgeo.org/grass/grass/trunk@57698 15284696-431f-4ddb-bdfa-cd5b030d7da7

gui/wxpython/gui_core/forms.py
@@ -89,7 +89,8 @@ from grass.pydispatch.signal import Signal
 from grass.script import core as grass
 from grass.script import task as gtask
 
-from gui_core.widgets import StaticWrapText, ScrolledPanel, ColorTablesComboBox
+from gui_core.widgets import StaticWrapText, ScrolledPanel, ColorTablesComboBox, \
+                             BarscalesComboBox, NArrowsComboBox
 from gui_core.ghelp   import HelpPanel
 from gui_core         import gselect
 from core             import gcmd
@@ -1060,6 +1061,15 @@ class CmdPanel(wx.Panel):
                             # list of values (combo)
                             if p['name'] == 'color':
                                 cb = ColorTablesComboBox(parent=which_panel, value=p.get('default',''),
+                                                         size=globalvar.DIALOG_COMBOBOX_SIZE,
+                                                         choices=valuelist)
+                            elif self.task.name == 'd.barscale':
+                                if p['name'] == 'style':
+                                    cb = BarscalesComboBox(parent=which_panel, value=p.get('default',''),
+                                                           size=globalvar.DIALOG_COMBOBOX_SIZE,
+                                                           choices=valuelist)
+                                elif p['name'] == 'north_arrow':
+                                    cb = NArrowsComboBox(parent=which_panel, value=p.get('default',''),
                                                          size=globalvar.DIALOG_COMBOBOX_SIZE,
                                                          choices=valuelist)
                             else:

commit 15f3a4a79ed21485e89d00b123dff32ae71e0f7b

wxGUI: fix bug introduced in r57698
git-svn-id: https://svn.osgeo.org/grass/grass/trunk@57701 15284696-431f-4ddb-bdfa-cd5b030d7da7

gui/wxpython/gui_core/forms.py
@@ -1063,7 +1063,7 @@ class CmdPanel(wx.Panel):
                                 cb = ColorTablesComboBox(parent=which_panel, value=p.get('default',''),
                                                          size=globalvar.DIALOG_COMBOBOX_SIZE,
                                                          choices=valuelist)
-                            elif self.task.name == 'd.barscale':
+                            elif self.task.name == 'd.barscale' and p['name'] in ('style', 'north_arrow'):
                                 if p['name'] == 'style':
                                     cb = BarscalesComboBox(parent=which_panel, value=p.get('default',''),
                                                            size=globalvar.DIALOG_COMBOBOX_SIZE,