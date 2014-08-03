grass-svn2git/grass

commit 92d37816d2101e689227125835557290eaebe8c6

wxGUI/animation: update format automatically after editing in preferences (merge from trunk, r61503)
git-svn-id: https://svn.osgeo.org/grass/grass/branches/releasebranch_7_0@61504 15284696-431f-4ddb-bdfa-cd5b030d7da7

gui/wxpython/animation/controller.py
@@ -189,6 +189,11 @@ class AnimationController(wx.EvtHandler):
 
         return None
 
+    def UpdateAnimations(self):
+        """Used sofar for updating slider time labels
+        after change of format"""'
+        self._setAnimations()
+
     def EditAnimations(self):
         # running = False
         # if self.timer.IsRunning():

gui/wxpython/animation/dialogs.py
@@ -1457,6 +1457,7 @@ class PreferencesDialog(PreferencesBaseDialog):
                  settings=UserSettings):
         PreferencesBaseDialog.__init__(self, parent=parent, giface=giface, title=title,
                                        settings=settings, size=(-1, 270))
+        self.formatChanged = Signal('PreferencesDialog.formatChanged')
 
         self._timeFormats = ['%Y-%m-%d %H:%M:%S',  # 2013-12-29 11:16:26
                              '%Y-%m-%d',  # 2013-12-29
@@ -1470,6 +1471,8 @@ class PreferencesDialog(PreferencesBaseDialog):
                              '%I %p',  # 11 AM
                              ]
         self._format = None
+        self._initFormat = self.settings.Get(group='animation', key='temporal',
+                                             subkey='format')
         # create notebook pages
         self._createGeneralPage(self.notebook)
         self._createTemporalPage(self.notebook)
@@ -1523,8 +1526,7 @@ class PreferencesDialog(PreferencesBaseDialog):
                       flag=wx.ALIGN_LEFT | wx.ALIGN_CENTER_VERTICAL, pos=(row, 0))
         self.tempFormat = wx.ComboBox(parent=panel, name='GetValue')
         self.tempFormat.SetItems(self._timeFormats)
-        self.tempFormat.SetValue(self.settings.Get(group='animation', key='temporal',
-                                                   subkey='format'))
+        self.tempFormat.SetValue(self._initFormat)
         self.winId['animation:temporal:format'] = self.tempFormat.GetId()
         gridSizer.Add(item=self.tempFormat, pos=(row, 1), flag=wx.ALIGN_RIGHT)
         self.infoTimeLabel = wx.StaticText(parent=panel)
@@ -1577,7 +1579,10 @@ class PreferencesDialog(PreferencesBaseDialog):
 
     def _updateSettings(self):
         self.tempFormat.SetValue(self._format)
-        return PreferencesBaseDialog._updateSettings(self)
+        PreferencesBaseDialog._updateSettings(self)
+        if self._format != self._initFormat:
+            self.formatChanged.emit()
+        return True
 
 
 def test():

gui/wxpython/animation/frame.py
@@ -296,6 +296,7 @@ class AnimationFrame(wx.Frame):
         if not self.dialogs['preferences']:
             dlg = PreferencesDialog(parent=self, giface=self._giface)
             self.dialogs['preferences'] = dlg
+            dlg.formatChanged.connect(lambda: self.controller.UpdateAnimations())
             dlg.CenterOnParent()
 
         self.dialogs['preferences'].ShowModal()

commit 1cd255381567a7ef585f83c3157e3b99dd3a7f2c

wxGUI: fix syntax error introduced by previous commit r61504
git-svn-id: https://svn.osgeo.org/grass/grass/branches/releasebranch_7_0@61506 15284696-431f-4ddb-bdfa-cd5b030d7da7

gui/wxpython/animation/controller.py
@@ -191,7 +191,7 @@ class AnimationController(wx.EvtHandler):
 
     def UpdateAnimations(self):
         """Used sofar for updating slider time labels
-        after change of format"""'
+        after change of format"""
         self._setAnimations()
 
     def EditAnimations(self):