grass-svn2git/grass

commit 0ac7cb84577d62511e9fdb6cb05205c2a52eb899

d.mon wx0: don't call g.gisenv in the watcher (it's too much invasive) - TODO: find better solution
git-svn-id: https://svn.osgeo.org/grass/grass/trunk@63475 15284696-431f-4ddb-bdfa-cd5b030d7da7

gui/wxpython/mapdisp/main.py
@@ -412,14 +412,19 @@ class MapApp(wx.App):
         """Redraw, if new layer appears (check's timestamp of
         cmdfile)
         """
-        try:
-            # GISBASE and other sytem enviromental variables can not be used
+        ###
+        ### TODO: fint better solution
+        ###
+        ### the check bellow disabled, it's invasive to call g.gisenv
+        ### in the watcher...
+        # try:
+            # GISBASE and other system enviromental variables can not be used
             # since the process inherited them from GRASS
             # raises exception when vaiable does not exists
-            grass.gisenv()['GISDBASE']
-        except KeyError:
-            self.timer.Stop()
-            return
+            ### grass.gisenv()['GISDBASE']
+        # except KeyError:
+        #    self.timer.Stop()
+        #    return
         
         # todo: events
         try:

commit 0ae406a2a4a0d2c139ef76d7eca0b0d9203302d3

fix typo in comment introduced in r63475
git-svn-id: https://svn.osgeo.org/grass/grass/trunk@63476 15284696-431f-4ddb-bdfa-cd5b030d7da7

gui/wxpython/mapdisp/main.py
@@ -413,10 +413,10 @@ class MapApp(wx.App):
         cmdfile)
         """
         ###
-        ### TODO: fint better solution
+        ### TODO: find a better solution
         ###
-        ### the check bellow disabled, it's invasive to call g.gisenv
-        ### in the watcher...
+        ### the check bellow disabled, it's too much invasive to call
+        ### g.gisenv in the watcher...
         # try:
             # GISBASE and other system enviromental variables can not be used
             # since the process inherited them from GRASS