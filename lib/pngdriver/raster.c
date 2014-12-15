grass-svn2git/grass

commit 721a993c0bb4460aca8536d487136c8a6a81691e

pngdriver: doxygen cosmetics
git-svn-id: https://svn.osgeo.org/grass/grass/trunk@63551 15284696-431f-4ddb-bdfa-cd5b030d7da7

lib/pngdriver/raster.c
@@ -73,9 +73,9 @@ static void alloc_buffers(void)
 /*!
   \brief Start drawing raster
 
-  \param mask
-  \param s
-  \param d
+  \param mask non-zero int for mask
+  \param s source (map) extent (left, right, top, bottom)
+  \param d destination (image) extent (left, right, top, bottom)
 */
 void PNG_begin_raster(int mask, int s[2][2], double fd[2][2])
 {

commit 2281eb04f646efc36465c5b2f80c3733731da6f8

fix small typo introduced in r63551
git-svn-id: https://svn.osgeo.org/grass/grass/trunk@63552 15284696-431f-4ddb-bdfa-cd5b030d7da7

lib/pngdriver/raster.c
@@ -75,7 +75,7 @@ static void alloc_buffers(void)
 
   \param mask non-zero int for mask
   \param s source (map) extent (left, right, top, bottom)
-  \param d destination (image) extent (left, right, top, bottom)
+  \param fd destination (image) extent (left, right, top, bottom)
 */
 void PNG_begin_raster(int mask, int s[2][2], double fd[2][2])
 {