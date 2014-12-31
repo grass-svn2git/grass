grass-svn2git/grass

commit 0873abfedf2020b7f85c48789d2913acf987a10a

d.vect.thematic: rename color options (https://trac.osgeo.org/grass/ticket/2409)
git-svn-id: https://svn.osgeo.org/grass/grass/trunk@63807 15284696-431f-4ddb-bdfa-cd5b030d7da7

scripts/d.vect.thematic/d.vect.thematic.py
@@ -106,7 +106,7 @@
 #% guisection: Color
 #%end
 #% option
-#% key: pointcolor
+#% key: point_color
 #% type: string
 #% label: Color for graduated points map
 #% description: GRASS named color or R:G:B triplet. Set color scheme to single color
@@ -115,7 +115,7 @@
 #% guisection: Color
 #%end
 #% option
-#% key: linecolor
+#% key: line_color
 #% type: string
 #% label: Color for graduated lines or point/area outlines
 #% description: GRASS named color or R:G:B triplet. Set color scheme to single color.
@@ -124,7 +124,7 @@
 #% guisection: Color
 #%end
 #% option
-#% key: startcolor
+#% key: start_color
 #% type: string
 #% label: Beginning color for custom color gradient
 #% description: Must be expressed as R:G:B triplet
@@ -133,7 +133,7 @@
 #% guisection: Color
 #%end
 #% option
-#% key: endcolor
+#% key: end_color
 #% type: string
 #% label: Ending color for custom color gradient
 #% description: Must be expressed as R:G:B triplet

commit 2b1248a20305b829d03c4f1c46d280ba70fe3b63

fix bug introduced in r63807
git-svn-id: https://svn.osgeo.org/grass/grass/trunk@63884 15284696-431f-4ddb-bdfa-cd5b030d7da7

scripts/d.vect.thematic/d.vect.thematic.py
@@ -234,18 +234,18 @@ def main():
     breakpoints = options['breakpoints']
     colorscheme = options['colorscheme']
     column = options['column']
-    endcolor = options['endcolor']
+    endcolor = options['end_color']
     group = options['group']
     layer = options['layer']
-    linecolor = options['linecolor']
+    linecolor = options['line_color']
     map = options['map']
     maxsize = options['maxsize']
     monitor = options['monitor']
     nint = options['nint']
-    pointcolor = options['pointcolor']
+    pointcolor = options['point_color']
     psmap = options['psmap']
     size = options['size']
-    startcolor = options['startcolor']
+    startcolor = options['start_color']
     themecalc = options['themecalc']
     themetype = options['themetype']
     type = options['type']