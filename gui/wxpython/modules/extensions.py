grass-svn2git/grass

commit 3c823ae8fec1da8a5215dc4982fb87822d6fb505

g.extension: avoid using option svnurl outside main fun
git-svn-id: https://svn.osgeo.org/grass/grass/trunk@65868 15284696-431f-4ddb-bdfa-cd5b030d7da7

scripts/g.extension/g.extension.py
@@ -398,18 +398,21 @@ def list_available_modules(url, mlist=None):
     """List modules available in the repository
 
     Tries to use XML metadata file first. Fallbacks to HTML page with a list.
+
+    :param url: URL of the directory (file name will be attached)
+    :param mlist: list only modules in this list
     """
-    url = url + "modules.xml"
-    grass.debug("url=%s" % url, 1)
+    file_url = url + "modules.xml"
+    grass.debug("url=%s" % file_url, 1)
     try:
-        tree = etree_fromurl(url, proxies=PROXIES)
+        tree = etree_fromurl(file_url, proxies=PROXIES)
     except ETREE_EXCEPTIONS:
         grass.warning(_("Unable to parse '%s'. Trying to scan"
-                        " SVN repository (may take some time)...") % url)
-        list_available_extensions_svn()
+                        " SVN repository (may take some time)...") % file_url)
+        list_available_extensions_svn(url)
         return
     except (HTTPError, URLError, IOError, OSError):
-        list_available_extensions_svn()
+        list_available_extensions_svn(url)
         return
 
     for mnode in tree.findall('task'):
@@ -430,14 +433,22 @@ def list_available_modules(url, mlist=None):
         else:
             print(name)
 
-# list extensions (scan SVN repo)
 
-
-def list_available_extensions_svn():
+# TODO: this is now broken/dead code, SVN is basically not used
+# fallback for Trac should parse Trac HTML page
+# this might be useful for potential SVN repos or anything
+# which would list the extensions/addons as list
+def list_available_extensions_svn(url):
     """List available extensions from HTML given by URL
 
+    Filename is generated based on the module class/family.
+
     ``<li><a href=...`` is parsed to find module names.
+    This works well for HTML page generated by Subversion.
+
+    :param url: a directory URL (filename will be attached)
     """
+    gscript.debug("list_available_extensions_svn(url=%s)" % url, 2)
     grass.message(_('Fetching list of extensions from'
                     ' GRASS-Addons SVN repository (be patient)...'))
     pattern = re.compile(r'(<li><a href=".+">)(.+)(</a></li>)', re.IGNORECASE)
@@ -455,7 +466,8 @@ def list_available_extensions_svn():
         modclass = expand_module_class_name(prefix)
         grass.verbose(_("Checking for '%s' modules...") % modclass)
 
-        url = '%s/%s' % (options['svnurl'], modclass)
+        # construct a full URL of a file
+        url = '%s/%s' % (url, modclass)
         grass.debug("url = %s" % url, debug=2)
         try:
             file_ = urlopen(url, proxies=PROXIES)
@@ -472,18 +484,23 @@ def list_available_extensions_svn():
             if name.split('.', 1)[0] == prefix:
                 print(name)
 
-    # get_wxgui_extensions()
+    # get_wxgui_extensions(url)
 
 
-def get_wxgui_extensions():
-    """Return list of extensions/addons in wxGUI directory at given URL"""
+# TODO: this is a dead code, not clear why not used, but seems not needed
+def get_wxgui_extensions(url):
+    """Return list of extensions/addons in wxGUI directory at given URL
+
+    :param url: a directory URL (filename will be attached)
+    """
     mlist = list()
     grass.debug('Fetching list of wxGUI extensions from '
                 'GRASS-Addons SVN repository (be patient)...')
     pattern = re.compile(r'(<li><a href=".+">)(.+)(</a></li>)', re.IGNORECASE)
     grass.verbose(_("Checking for '%s' modules...") % 'gui/wxpython')
 
-    url = '%s/%s' % (options['svnurl'], 'gui/wxpython')
+    # construct a full URL of a file
+    url = '%s/%s' % (url, 'gui/wxpython')
     grass.debug("url = %s" % url, debug=2)
     file_ = urlopen(url, proxies=PROXIES)
     if not file_:
@@ -1584,6 +1601,8 @@ def main():
     if sys.platform != "win32":
         check_progs()
 
+    original_url = options['svnurl']
+
     # manage proxies
     global PROXIES
     if options['proxy']:
@@ -1600,9 +1619,9 @@ def main():
         # using dummy module, we don't need any module URL now,
         # but will work only as long as the function does not check
         # if the URL is actually valid or something
-        source, url = resolve_source_code(name='r.dummy',
-                                          url=options['svnurl'])
-        xmlurl = resolve_xmlurl_prefix(options['svnurl'], source=source)
+        source, url = resolve_source_code(name='dummy',
+                                          url=original_url)
+        xmlurl = resolve_xmlurl_prefix(original_url, source=source)
         list_available_extensions(xmlurl)
         return 0
     elif flags['a']:
@@ -1620,8 +1639,8 @@ def main():
     if options['operation'] == 'add':
         check_dirs()
         source, url = resolve_source_code(name=options['extension'],
-                                          url=options['svnurl'])
-        xmlurl = resolve_xmlurl_prefix(options['svnurl'], source=source)
+                                          url=original_url)
+        xmlurl = resolve_xmlurl_prefix(original_url, source=source)
         install_extension(source=source, url=url, xmlurl=xmlurl)
     else:  # remove
         remove_extension(force=flags['f'])

commit 32cea1accf759db236e721ac8b2e8293adc6410c

g.extension: rename option svnurl to url
The name url is not 100% friendly for all when used for local files but source
(with unclear meaning) and input (usually an actual file to process) don't
seem better.

Also clean up in the GUI wrapper, document tests and fix bug in listing from HTML
(introduced in r65868).


git-svn-id: https://svn.osgeo.org/grass/grass/trunk@65869 15284696-431f-4ddb-bdfa-cd5b030d7da7

gui/wxpython/modules/extensions.py
@@ -48,7 +48,7 @@ class InstallExtensionWindow(wx.Frame):
         self.panel = wx.Panel(parent = self, id = wx.ID_ANY)
 
         self.repoBox = wx.StaticBox(parent = self.panel, id = wx.ID_ANY,
-                                    label = " %s " % _("Repository"))
+                                    label = " %s " % _("Repository (leave empty to use the official one)"))
         self.treeBox = wx.StaticBox(parent = self.panel, id = wx.ID_ANY,
                                     label = " %s " % _("List of extensions - double-click to install"))
         
@@ -86,9 +86,9 @@ class InstallExtensionWindow(wx.Frame):
                 continue
             self.options[name] = wx.CheckBox(parent = self.panel, id = wx.ID_ANY,
                                              label = desc)
-        defaultUrl = 'http://svn.osgeo.org/grass/grass-addons/grass7'
-        self.repo.SetValue(task.get_param(value = 'svnurl').get('default', defaultUrl))
-        
+        defaultUrl = ''  # default/official one will be used when option empty
+        self.repo.SetValue(task.get_param(value='url').get('default', defaultUrl))
+
         self.statusbar = self.CreateStatusBar(number = 1)
         
         self.btnFetch = wx.Button(parent = self.panel, id = wx.ID_ANY,
@@ -179,7 +179,7 @@ class InstallExtensionWindow(wx.Frame):
                     flags.append('--%s' % key)
         
         return ['g.extension'] + flags + ['extension=' + name,
-                                          'svnurl=' + self.repo.GetValue().strip()]
+                                          'url=' + self.repo.GetValue().strip()]
 
     def OnFetch(self, event):
         """Fetch list of available extensions"""
@@ -310,7 +310,7 @@ class ExtensionTreeModelBuilder:
         else:
             flags = 'l'
         retcode, ret, msg = RunCommand('g.extension', read = True, getErrorMsg = True,
-                                       svnurl = url,
+                                       url=url,
                                        flags = flags, quiet = True)
         if retcode != 0:
             raise GException(_("Unable to load extensions. %s") % msg)

lib/gis/renamed_options
@@ -87,6 +87,8 @@ db.select|vs:vertical_separator
 #########################################
 ### General map management module changes
 #########################################
+# g.extension
+g.extension|svnurl:url
 # g.mapset
 g.mapset|gisdbase:dbase
 # g.mapsets

scripts/g.extension/g.extension.py
@@ -4,9 +4,10 @@
 #
 # MODULE:       g.extension
 # AUTHOR(S):    Markus Neteler
-#               Pythonized & upgraded for GRASS 7 by Martin Landa <landa.martin gmail.com>
-# PURPOSE:      Tool to download and install extensions from GRASS Addons SVN into
-#               local GRASS installation
+#               Martin Landa <landa.martin gmail com> (Pythonized & upgraded for GRASS 7)
+#               Vaclav Petras <wenzeslaus gmail com> (support for general sources)
+# PURPOSE:      Tool to download and install extensions into local installation
+#               
 # COPYRIGHT:    (C) 2009-2014 by Markus Neteler, and the GRASS Development Team
 #
 #               This program is free software under the GNU General
@@ -19,10 +20,12 @@
 
 #%module
 #% label: Maintains GRASS Addons extensions in local GRASS installation.
-#% description: Downloads, installs extensions from GRASS Addons SVN repository into local GRASS installation or removes installed extensions.
+#% description: Downloads and installs extensions from GRASS Addons repository or other source into the local GRASS installation or removes installed extensions.
 #% keyword: general
 #% keyword: installation
 #% keyword: extensions
+#% keyword: addons
+#% keyword: download
 #%end
 
 #%option
@@ -42,10 +45,11 @@
 #% answer: add
 #%end
 #%option
-#% key: svnurl
+#% key: url
 #% type: string
 #% key_desc: url
-#% description: SVN Addons repository URL
+#% label: URL or directory to get the extension from
+#% description: The official repository is used by default. User can specify a ZIP file, directory or a repository on common hosting services. If not identified, Subversion repository is assumed. See manual for all options.
 #%end
 #%option
 #% key: prefix
@@ -438,10 +442,12 @@ def list_available_modules(url, mlist=None):
 # fallback for Trac should parse Trac HTML page
 # this might be useful for potential SVN repos or anything
 # which would list the extensions/addons as list
+# TODO: fail when nothing is accessible
 def list_available_extensions_svn(url):
     """List available extensions from HTML given by URL
 
     Filename is generated based on the module class/family.
+    This works well for the structure which is in grass-addons repository.
 
     ``<li><a href=...`` is parsed to find module names.
     This works well for HTML page generated by Subversion.
@@ -467,12 +473,12 @@ def list_available_extensions_svn(url):
         grass.verbose(_("Checking for '%s' modules...") % modclass)
 
         # construct a full URL of a file
-        url = '%s/%s' % (url, modclass)
-        grass.debug("url = %s" % url, debug=2)
+        file_url = '%s/%s' % (url, modclass)
+        grass.debug("url = %s" % file_url, debug=2)
         try:
-            file_ = urlopen(url, proxies=PROXIES)
+            file_ = urlopen(file_url, proxies=PROXIES)
         except (HTTPError, IOError, OSError):
-            grass.debug(_("Unable to fetch '%s'") % url, debug=1)
+            grass.debug(_("Unable to fetch '%s'") % file_url, debug=1)
             continue
 
         for line in file_.readlines():
@@ -1498,6 +1504,7 @@ def resolve_known_host_service(url):
         return None, None
 
 
+# TODO: add also option to enforce the source type
 def resolve_source_code(url=None, name=None):
     """Return type and URL or path of the source code
 
@@ -1601,7 +1608,7 @@ def main():
     if sys.platform != "win32":
         check_progs()
 
-    original_url = options['svnurl']
+    original_url = options['url']
 
     # manage proxies
     global PROXIES

scripts/g.extension/testsuite/test_addons_modules.py
@@ -51,8 +51,8 @@ class TestModulesMetadata(TestCase):
     url = 'file://' + os.path.abspath('data')
 
     def test_listing(self):
-        """Test if results is in expected limits"""
-        module = SimpleModule('g.extension', flags='l', svnurl=self.url)
+        """List individual extensions/modules/addons"""
+        module = SimpleModule('g.extension', flags='l', url=self.url)
         self.assertModule(module)
         stdout = module.outputs.stdout
         self.assertMultiLineEqual(stdout, MODULES_OUTPUT)
@@ -87,35 +87,35 @@ class TestModulesFromDifferentSources(TestCase):
         silent_rmtree(self.install_prefix)
 
     def test_directory_install(self):
-        """Test if results is in expected limits"""
+        """Test installing extension from directory"""
         self.assertModule('g.extension', extension='r.plus.example',
-                          svnurl=os.path.join(self.path, 'r.plus.example'),
+                          url=os.path.join(self.path, 'r.plus.example'),
                           prefix=self.install_prefix)
         # TODO: this is wrong for MS Win
         for file in self.files:
             self.assertFileExists(file)
 
     def test_targz_install(self):
-        """Test if results is in expected limits"""
+        """Test installing extension from local .tar.gz"""
         self.assertModule('g.extension', extension='r.plus.example',
-                          svnurl=os.path.join(self.path,
+                          url=os.path.join(self.path,
                                               'r.plus.example.tar.gz'),
                           prefix=self.install_prefix)
         for file in self.files:
             self.assertFileExists(file)
 
     def test_remote_targz_without_dir_install(self):
-        """Test if results is in expected limits"""
+        """Test installing extension from (remote) .tar.gz without main dir"""
         self.assertModule('g.extension', extension='r.plus.example',
-                          svnurl=self.url + '/' + 'r.plus.example_sep.tar.gz',
+                          url=self.url + '/' + 'r.plus.example_sep.tar.gz',
                           prefix=self.install_prefix, verbose=True)
         for file in self.files:
             self.assertFileExists(file)
 
     def test_remote_zip_install(self):
-        """Test if results is in expected limits"""
+        """Test installing extension from .zip specified by URL (local)"""
         self.assertModule('g.extension', extension='r.plus.example',
-                          svnurl=self.url + '/' + 'r.plus.example.zip',
+                          url=self.url + '/' + 'r.plus.example.zip',
                           prefix=self.install_prefix)
         for file in self.files:
             self.assertFileExists(os.path.join(file))

scripts/g.extension/testsuite/test_addons_toolboxes.py
@@ -41,8 +41,8 @@ class TestToolboxesMetadata(TestCase):
     url = 'file://' + os.path.abspath('data')
 
     def test_listing(self):
-        """Test if results is in expected limits"""
-        module = SimpleModule('g.extension', flags='lt', svnurl=self.url)
+        """List toolboxes and their content"""
+        module = SimpleModule('g.extension', flags='lt', url=self.url)
         self.assertModule(module)
         stdout = module.outputs.stdout
         self.assertLooksLike(stdout, FULL_TOOLBOXES_OUTPUT)