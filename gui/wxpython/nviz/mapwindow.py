grass-svn2git/grass

commit 80c5217fe8e48f0131b75fa41cba07a5873f8d10

wxGUI/nviz: pass only existing maps for surface/3d raster attributes to avoid crashing (merge from trunk, https://trac.osgeo.org/grass/changeset/64300)
git-svn-id: https://svn.osgeo.org/grass/grass/branches/releasebranch_7_0@64301 15284696-431f-4ddb-bdfa-cd5b030d7da7

gui/wxpython/nviz/mapwindow.py
@@ -1917,9 +1917,11 @@ class GLWindow(MapWindowBase, glcanvas.GLCanvas):
                 elif attrb ==  'transp':
                     self._display.UnsetSurfaceTransp(id) 
             else:
-                if type(value) == types.StringType and \
-                        len(value) <=  0: # ignore empty values (TODO: warning)
-                    continue
+                if type(value) == types.StringType:
+                    if len(value) == 0:  # ignore empty values (TODO: warning)
+                        continue
+                    if map and not grass.find_file(value, element='cell')['fullname']:
+                        continue
                 if attrb ==  'color':
                     self._display.SetSurfaceColor(id, map, str(value))
                 elif attrb ==  'mask':
@@ -2025,9 +2027,11 @@ class GLWindow(MapWindowBase, glcanvas.GLCanvas):
                     elif attrb ==  'transp':
                         self._display.UnsetIsosurfaceTransp(id, isosurfId) 
                 else:
-                    if type(value) == types.StringType and \
-                            len(value) <=  0: # ignore empty values (TODO: warning)
-                        continue
+                    if type(value) == types.StringType:
+                        if len(value) == 0:  # ignore empty values (TODO: warning)
+                            continue
+                        if map and not grass.find_file(value, element='grid3')['fullname']:
+                            continue
                     elif attrb ==  'color':
                         self._display.SetIsosurfaceColor(id, isosurfId, map, str(value))
                     elif attrb ==  'mask':

commit 5c0ce94489225f20a483e78b31ecf15e8cbc9c29

wxGUI/nviz: fix coloring isosurfaces, broken in r64301
git-svn-id: https://svn.osgeo.org/grass/grass/trunk@66588 15284696-431f-4ddb-bdfa-cd5b030d7da7

gui/wxpython/nviz/mapwindow.py
@@ -2035,7 +2035,7 @@ class GLWindow(MapWindowBase, glcanvas.GLCanvas):
                             continue
                         if map and not grass.find_file(value, element='grid3')['fullname']:
                             continue
-                    elif attrb ==  'color':
+                    if attrb ==  'color':
                         self._display.SetIsosurfaceColor(id, isosurfId, map, str(value))
                     elif attrb ==  'mask':
                         # TODO: invert mask