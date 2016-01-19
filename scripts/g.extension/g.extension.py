grass-svn2git/grass

commit 11d54d544cb988c595b581a28a37768a6c964922

g.extension: fix wingrass link, see https://lists.osgeo.org/pipermail/grass-dev/2016-January/078472.html
git-svn-id: https://svn.osgeo.org/grass/grass/branches/releasebranch_7_0@67598 15284696-431f-4ddb-bdfa-cd5b030d7da7

scripts/g.extension/g.extension.py
@@ -9,6 +9,8 @@
 #               local GRASS installation
 # COPYRIGHT:    (C) 2009-2014 by Markus Neteler, and the GRASS Development Team
 #
+# COPYRIGHT:    (C) 2009-2016 by Markus Neteler, and the GRASS Development Team
+#
 #               This program is free software under the GNU General
 #               Public License (>=v2). Read the file COPYING that
 #               comes with GRASS for details.
@@ -704,11 +706,12 @@ def install_extension_win(name):
         platform = build_platform
     else:
         platform = 'x86'
-    url = "http://wingrass.fsv.cvut.cz/%(platform)s/" \
-          "grass%(major)s%(minor)s/addons/" \
-          "grass-%(major)s.%(minor)s.%(patch)s/" % \
-        {'platform' : platform, 'major': version[0],
-         'minor': version[1], 'patch': version[2]}
+    url = "http://wingrass.fsv.cvut.cz/" \
+          "grass%(major)s%(minor)s/%(platform)s/addons/" \
+          "grass-%(major)s.%(minor)s.%(patch)s" % \
+          {'platform' : platform,
+           'major': version[0], 'minor': version[1],
+           'patch': version[2]}
     
     grass.debug("url=%s" % url, 1)
 

commit 01a628db8755d6c7f71d4e65191d4cb49151e3b1

g.extension: fix bug introduced in r67598
git-svn-id: https://svn.osgeo.org/grass/grass/branches/releasebranch_7_0@67610 15284696-431f-4ddb-bdfa-cd5b030d7da7

scripts/g.extension/g.extension.py
@@ -708,7 +708,7 @@ def install_extension_win(name):
         platform = 'x86'
     url = "http://wingrass.fsv.cvut.cz/" \
           "grass%(major)s%(minor)s/%(platform)s/addons/" \
-          "grass-%(major)s.%(minor)s.%(patch)s" % \
+          "grass-%(major)s.%(minor)s.%(patch)s/" % \
           {'platform' : platform,
            'major': version[0], 'minor': version[1],
            'patch': version[2]}